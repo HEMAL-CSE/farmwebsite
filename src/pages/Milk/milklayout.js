ff3175b42afc2277bc6a973e7efe8d616c5802bd
Fyaz Rayat <fyazrayat@gmail.com>
2024-10-27 17:30:15 +0600

    c

--- src/pages/Milk/milklayout.js
+++ src/pages/Milk/milklayout.js
@@ -1,7 +1,120 @@
 import React from 'react'
+import './cow.css'
+import { BsPeople, BsSpeedometer } from 'react-icons/bs'
+import { BiArrowFromBottom, BiArrowToBottom, BiChart, BiHealth, BiMoney, BiPlus, BiSolidArrowToBottom } from 'react-icons/bi'
+import { FaCow, FaSeedling, FaUserDoctor } from 'react-icons/fa6'
+import { GiBottomRight3dArrow, GiDoctorFace } from 'react-icons/gi'
+import { MdReport } from 'react-icons/md'
+import 'bootstrap/js/dist/dropdown'
+import 'bootstrap/js/dist/collapse'
+import { CgArrowBottomLeft } from 'react-icons/cg'
+import { useLocation } from 'react-router-dom'
+import Sidebar from '../../Components/Sidebar'
+export const Milklayout = ({children}) => {
+  const sidebarElements = [
+    {
+        name: 'Purchase',
+        pathname: '/milk/purchase',
+        icon: BiPlus,
+        submenu: []
+    },
+    
+    {
+        name: 'Production',
+        pathname: '/milk/production',
+        icon: FaCow,
+        submenu: []
+    },
+    {
+        name: 'Expenses',
+        pathname: '',
+        icon: BiMoney,
+        submenu: [
+            {
+                name: 'Feed',
+                pathname: '/milk/feed'
+            },
 
-export const Milklayout = () => {
+            {
+                name: 'Labour',
+                pathname: '/milk/labour'
+            },
+
+            {
+                name: 'Labour Payment',
+                pathname: '/milk/labour_payment'
+            },
+            {
+                name: 'Others',
+                pathname: '/milk/others'
+            },
+            {
+                name: 'Others Payment',
+                pathname: '/milk/others_payment'
+            }
+        ]
+    },
+
+    {
+        name: 'Treatment',
+        pathname: '',
+        icon: FaUserDoctor,
+        submenu: [
+            {
+                name: 'Doctors',
+                pathname: '/milk/doctors'
+            },
+            {
+                name: 'Treatment',
+                pathname: '/milk/treatment'
+            }
+        ]
+    },
+
+    {
+        name: 'Customer List',
+        pathname: '/milk/customers',
+        icon: BiMoney,
+        submenu: []
+    },
+
+    {
+        name: 'Healthcare',
+        pathname: '',
+        icon: BiHealth,
+        submenu: [
+            {
+                name: 'Vaccines',
+                pathname: '/milk/vaccines'
+            },
+            {
+                name: 'Medicine',
+                pathname: '/milk/medicine'
+            }
+        ]
+    },
+    {
+        name: 'Feeding',
+        pathname: '/milk/feeding',
+        icon: FaSeedling,
+        submenu: []
+    },
+    {
+        name: 'Report',
+        pathname: '/milk/report',
+        icon: MdReport,
+        submenu: []
+    }
+    
+  ]
+  
   return (
-    <div>M</div>
+    <div className='layout d-flex'>
+      <Sidebar className='calf' elements={sidebarElements} name={'Cow'} />
+      <div className='ms-4 vw-100 d-flex justify-content-center'>
+        {children}
+      </div>
+      
+    </div>
   )
 }
